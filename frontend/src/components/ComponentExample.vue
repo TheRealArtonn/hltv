15124e611719cb50f9fac6d6fc106f2b8cf8369a
TheRealArtonn <61601482+TheRealArtonn@users.noreply.github.com>
2022-05-09 14:07:45 +0200

    fixed some config files, so we can use 4 tabs. Waste of time? NOT

--- frontend/src/components/ComponentExample.vue
+++ frontend/src/components/ComponentExample.vue
@@ -1,44 +1,44 @@
 <template>
-  <div class="votes">
-    <div class="votes__icon"></div>
-    <div class="likes">{{ item.likes }}</div>
-    <div class="votes__icon votes__icon--down">{{ item.dislikes }}</div>
-    <div class="dislikes">1</div>
-  </div>
+    <div class="votes">
+        <div class="votes__icon"></div>
+        <div class="likes">{{ item.likes }}</div>
+        <div class="votes__icon votes__icon--down">{{ item.dislikes }}</div>
+        <div class="dislikes">1</div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "voteModule",
-  props: ["item"],
+    name: "voteModule",
+    props: ["item"],
 };
 </script>
 
 <style lang="scss" scoped>
 .votes {
-  width: 180px;
-  height: 50px;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  padding-right: 15px;
+    width: 180px;
+    height: 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding-right: 15px;
 }
 .votes__icon {
-  width: 40px;
-  height: 40px;
-  background-image: url("@/assets/img/icons/vote-hollow.svg");
-  background-size: 40px;
-  background-repeat: no-repeat;
-  background-position: center;
-  transition: background-image 0.5s ease;
+    width: 40px;
+    height: 40px;
+    background-image: url("@/assets/img/icons/vote-hollow.svg");
+    background-size: 40px;
+    background-repeat: no-repeat;
+    background-position: center;
+    transition: background-image 0.5s ease;
 }
 
 .votes__icon--down {
-  transform: rotate(180deg);
-  margin-top: 4px;
+    transform: rotate(180deg);
+    margin-top: 4px;
 }
 
 .votes__icon--active {
-  background-image: url("@/assets/img/icons/vote-filled.svg");
+    background-image: url("@/assets/img/icons/vote-filled.svg");
 }
 </style>

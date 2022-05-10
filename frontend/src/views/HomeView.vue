0035e0da2fe5d1dd6da93ef7d1fb8b739be574c4
TheRealArtonn <61601482+TheRealArtonn@users.noreply.github.com>
2022-05-10 12:02:59 +0200

    removing repeating css

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -18,22 +18,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.grid {
-    width: 1400px;
-    height: auto;
-    background-color: blanchedalmond;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-auto-rows: 70px;
-    gap: 25px;
-    padding: 25px;
-}
-
-.grid__item {
-    min-width: 450px;
-    height: 100%;
-}
-
 .grid__item--events {
     grid-column: 1 / 1;
     grid-row: 1 / span 9;

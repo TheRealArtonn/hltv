9f85b0a9846c684f1f264f1088b0ee000c64ee3b
dado26516 <dado26516@edu.ucl.dk>
2022-05-12 10:08:08 +0200

    module_template

---
+++ frontend/src/views/ModuleTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="module-template-wrapper">
+    <div class="module-template-heading">
+    <h3 class="module-heading">Jeg er bange</h3>
+    </div>
+
+    <div class="module-data-wrapper">
+        <p>ladida</p>
+    </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/_variables.scss";
+
+    .module-template-wrapper {
+        display: flex;
+        flex-direction: column;
+        border-radius: 5px;
+        .module-template-heading{
+            background: $header-gradient-background-color;
+        }
+    }
+</style>

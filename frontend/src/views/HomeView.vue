ec73f19b87b5c1d035557c2f50f5ec30141410c7
TheRealArtonn <61601482+TheRealArtonn@users.noreply.github.com>
2022-05-12 14:28:47 +0200

    ny module

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,25 +1,33 @@
 <template>
     <div class="home grid">
         <event-module :item="value" class="grid__item grid__item--events" />
+        <lorte-module :item="sort" class="grid__item grid__item--lorte" />
     </div>
 </template>
 
 <script>
 import EventModule from "@/components/EventModule.vue";
+import LorteModule from "@/components/LorteModule.vue";
 export default {
     name: "HomeView",
     data: function () {
         return {
             value: "dildo",
+            sort: "lorte is",
         };
     },
-    components: { EventModule },
+    components: { EventModule, LorteModule },
 };
 </script>
 
 <style lang="scss" scoped>
 .grid__item--events {
-    grid-column: 1 / 1;
+    grid-column: 1 / span 1;
     grid-row: 1 / span 9;
+}
+
+.grid__item--lorte {
+    grid-column: 2 / span 2;
+    grid-row: 1 / span 4;
 }
 </style>

50a66049b0d53f1fe42c2e868643d996d18e8a0d
TheRealArtonn <61601482+TheRealArtonn@users.noreply.github.com>
2022-05-18 11:37:43 +0200

    Creating ongoingevent header, adding config

---
+++ frontend/src/components/OngoingEventModule.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="module">
+        <h2 class="module__header">{{ moduleName }}</h2>
+        <div class="module__body">
+            <h3 class="body__title">Ongoing events</h3>
+            <div class="body__unfocused-title body__border-above">
+                Relevant to you
+            </div>
+            {{ value }}
+        </div>
+        <div class="module__footer">Show more</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TryModule",
+    props: ["value"],
+    data: function () {
+        return {
+            moduleName: "Pis lort",
+        };
+    },
+};
+</script>
+
+// Write all of your css below, just like what you would do normally.
+<style lang="scss" scoped>
+//Import needed css partials, in this case we needed the variables.
+@import "@/assets/styles/_variables.scss";
+</style>

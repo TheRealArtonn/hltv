63b6eb528f94bf7eadb7780eb01e2921de0a6f11
TheRealArtonn <61601482+TheRealArtonn@users.noreply.github.com>
2022-05-25 14:03:21 +0200

    Currently working on optimized update of component

--- src/components/EventsPresetSetup.vue
+++ src/components/EventsPresetSetup.vue
@@ -4,25 +4,25 @@
             :itemList="itemList"
             class="grid__item grid__item--events"
         />
-        <ongoing-event-module class="grid__item grid__item--ongoingevent" />
+        <specific-event-module class="grid__item grid__item--specific-event" />
     </div>
 </template>
 
 <script>
 import EventModule from "@/components/EventModule.vue";
-import OngoingEventModule from "@/components/OngoingEventModule.vue";
 
 //Henter json filen, med data til brug. Længere nede vil du finde itemList, som tager imod jsonfil indhold.
 import EventsJsonFile from "@/assets/json/events.json";
+import SpecificEventModule from "./SpecificEventModule.vue";
 
 export default {
-    name: "HomeView",
+    name: "EventsPresetSetup",
     data: function () {
         return {
             itemList: EventsJsonFile,
         };
     },
-    components: { EventModule, OngoingEventModule },
+    components: { EventModule, SpecificEventModule },
 };
 </script>
 
@@ -32,8 +32,7 @@ export default {
     grid-row: 1 / span 14;
 }
 
-.grid__item--ongoingevent {
+.grid__item--specific-event {
     grid-column: 2 / span 2;
-    grid-row: 1 / span 4;
 }
 </style>
---
+++ src/components/SpecificEventModule.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="events-preset grid grid--two-columns">
+        <ongoing-event-module class="grid__item grid__item--ongoingevent" />
+    </div>
+</template>
+
+<script>
+import OngoingEventModule from "@/components/OngoingEventModule.vue";
+
+export default {
+    name: "SpecificEventModule",
+    props: ["value"],
+    data: function () {
+        return {
+            moduleName: "Pis lort",
+        };
+    },
+    components: { OngoingEventModule },
+};
+</script>
+
+// Write all of your css below, just like what you would do normally.
+<style lang="scss" scoped>
+//Import needed css partials, in this case we needed the variables.
+@import "@/assets/styles/_variables.scss";
+
+.grid--two-columns {
+    grid-template-columns: repeat(2, 1fr);
+    grid-column: 2 / span 2;
+    padding: 0px;
+}
+
+.grid__item--ongoingevent {
+    grid-column: 2 / span 1;
+}
+</style>

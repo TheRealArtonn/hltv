8750c591f68b3dbcd3aa7e644f55b13d173b25ba
dado26516 <dado26516@edu.ucl.dk>
2022-05-26 15:17:45 +0200

    zindex added
    Co-Authored-By: Kunchorn <70174665+Kunchorn@users.noreply.github.com>

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -49,6 +49,7 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/styles/_variables.scss";
 .modal-container {
+    z-index: 5;
     width: 100%;
     height: 100%;
     background: black;

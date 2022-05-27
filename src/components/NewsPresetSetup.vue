de6e844be62a6059f81c0af56338143bf087b3b4
TheRealArtonn <61601482+TheRealArtonn@users.noreply.github.com>
2022-05-27 11:01:08 +0200

    css and component example

--- src/components/NewsPresetSetup.vue
+++ src/components/NewsPresetSetup.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="news-preset grid">
-        <!-- Smid components her ind -->
         <try-module class="grid__item grid__item--trymodule" />
-        <span>test</span>
     </div>
 </template>
 
@@ -18,12 +16,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.grid__item--events {
-    grid-column: 1 / span 1;
-    grid-row: 1 / span 14;
-}
 .grid__item--trymodule {
     grid-column: 1 / span 1;
-    grid-row: 1 / span 6;
+    grid-row: 1 / span 4;
 }
 </style>
--- src/components/TryModule.vue
+++ src/components/TryModule.vue
@@ -1,38 +1,28 @@
 <template>
     <div class="module">
-        <h2 class="module__header">fisk</h2>
-        <div class="module__body">
-            <!-- <div class="body__title">fisk</div> -->
-            <div class="body__unfocused-title"></div>
-        </div>
-        <div class="module__footer">show more</div>
-    </div>
-    <!-- <div class="module">
-        <h2 class="module__header">{{ moduleName }}</h2>
+        <h2 class="module__header">Pis lort</h2>
         <div class="module__body">
             <h3 class="body__title">Ongoing events</h3>
             <div class="body__unfocused-title body__border-above">
                 Relevant to you
             </div>
-            {{ value }}
+            <div class="list-item">tekst</div>
+            <div class="list-item">tekst</div>
+            <div class="list-item">tekst</div>
         </div>
         <div class="module__footer">Show more</div>
-    </div> -->
+    </div>
 </template>
 
 <script>
 export default {
     name: "TryModule",
-    props: ["value"],
     data: function () {
-        return {
-            moduleName: "Pis lort",
-        };
+        return {};
     },
 };
 </script>
 
-// Write all of your css below, just like what you would do normally.
 <style lang="scss" scoped>
 //Import needed css partials, in this case we needed the variables.
 @import "@/assets/styles/_variables.scss";

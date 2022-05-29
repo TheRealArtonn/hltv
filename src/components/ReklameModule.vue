429bc04e02b77a526b16a0fc3dd799022ede9943
TheRealArtonn <61601482+TheRealArtonn@users.noreply.github.com>
2022-05-29 15:04:18 +0200

    reklame

---
+++ src/components/ReklameModule.vue
@@ -0,0 +1,23 @@
+<template>
+    <img class="reklame" src="@/assets/img/ads.jpg" />
+</template>
+
+<script>
+export default {
+    name: "ReklameModule",
+    data: function () {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+//Import needed css partials, in this case we needed the variables.
+@import "@/assets/styles/_variables.scss";
+
+.reklame {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
